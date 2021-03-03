f020b96cd3ae422c272c9ee6c340d434d73b66e6
Anastasia <nastenaprokopeva78@gmail.com>
2021-03-03 22:25:23 +0300

    внесла изменения в вывод информации о тарифах

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,23 @@ public class Main {
     public static void main(String[] args) {
         MobileTariff closePeopleOne = new MobileTariff();
         closePeopleOne.name = "Близкие люди 1";
-        closePeopleOne.digitalSimCard = "Подерживает eSIM";
+        closePeopleOne.digitalSimCard = "Поддерживает eSIM";
         closePeopleOne.internetTraffic = 15;
         closePeopleOne.freeMinutes = 400;
         closePeopleOne.freeMessages = 300;
         closePeopleOne.price = 350;
-        System.out.println(closePeopleOne.name);
-        System.out.println(closePeopleOne.digitalSimCard);
-        System.out.println(closePeopleOne.internetTraffic + " ГБ");
-        System.out.println(closePeopleOne.freeMinutes + " МИН");
-        System.out.println(closePeopleOne.freeMessages + " SMS");
-        System.out.println(closePeopleOne.price + " РУБ/МЕС");
+        System.out.println(closePeopleOne);
+
 
         MobileTariff closePeopleTwo = new MobileTariff();
-        closePeopleOne.name = "Близкие люди 2";
-        closePeopleOne.digitalSimCard = "Подерживает eSIM";
-        closePeopleOne.internetTraffic = 30;
-        closePeopleOne.freeMinutes = 600;
-        closePeopleOne.freeMessages = 300;
-        closePeopleOne.oldPrice = 400;
-        closePeopleOne.price = 320;
-        System.out.println(closePeopleOne.name);
-        System.out.println(closePeopleOne.digitalSimCard);
-        System.out.println(closePeopleOne.internetTraffic + " ГБ");
-        System.out.println(closePeopleOne.freeMinutes + " МИН");
-        System.out.println(closePeopleOne.freeMessages + " SMS");
-        System.out.println(closePeopleOne.oldPrice + " РУБ/МЕС");
-        System.out.println(closePeopleOne.price + " РУБ/МЕС на год");
-
+        closePeopleTwo.name = "Близкие люди 2";
+        closePeopleTwo.digitalSimCard = "Поддерживает eSIM";
+        closePeopleTwo.internetTraffic = 30;
+        closePeopleTwo.freeMinutes = 600;
+        closePeopleTwo.freeMessages = 300;
+        closePeopleTwo.oldPrice = 400;
+        closePeopleTwo.discount = 20;
+        closePeopleTwo.price = 320;
+        System.out.println(closePeopleTwo);
     }
 }
--- src/main/java/MobileTariff.java
+++ src/main/java/MobileTariff.java
@@ -1,10 +1,23 @@
 public class MobileTariff {
     String name;
     String digitalSimCard;
-    int oldPrice;
-    int price;
     int internetTraffic;
-    int freeMessages;
     int freeMinutes;
+    int freeMessages;
+    int oldPrice;
+    int discount;
+    int price;
 
+    @Override
+    public String toString() {
+        return name  +
+                "\n" + digitalSimCard +
+                "\n" + internetTraffic + " ГБ" +
+                "\n" + freeMinutes + " МИН" +
+                "\n" + freeMessages + " SMS" +
+                "\n" + oldPrice + " РУБ/МЕС" +
+                "\n" + "-" + discount + "% на год" +
+                "\n" + price + " РУБ/МЕС" +
+                "\n";
+    }
 }
